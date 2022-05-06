727d2b9f419ef915222fb67dff2cc18a75364563
Madhavi Deshpande <deshpande.v.madhavi@gmail.com>
2022-05-06 09:13:10 +0530

    Add files via upload

---
+++ src/Utilities/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LoadCSV
+{
+    public static class EMail
+    {
+        // Regular expression used to validate an Email.  
+        public const string emailRegex = @"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        public static bool IsEmail(string email)
+        {
+            if (email != null) return Regex.IsMatch(email, emailRegex);
+            else
+                return false;
+        }
+    }
+}
--- src/Utilities/PhoneNumber.cs
+++ src/Utilities/PhoneNumber.cs
@@ -1,1 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
 
+namespace LoadCSV
+{
+    public  static class PhoneNumber
+    {
+        // Regular expression used to validate a phone number.  
+        public const string phoneNumberRegex = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+
+        public static bool IsPhoneNbr(string number)
+        {
+            if (number != null) return Regex.IsMatch(number, phoneNumberRegex);
+            else return false;
+        }
+    }
+
+   
+}
