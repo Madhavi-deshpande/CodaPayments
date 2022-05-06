7a9d0512b2b253ec3a5448a7cc072478b8364cf8
Madhavi Deshpande <deshpande.v.madhavi@gmail.com>
2022-05-06 09:11:22 +0530

    Load UI and Code behind

---
+++ src/LoadCSV/LoadForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LoadCSV
+{
+    partial class LoadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browse = new System.Windows.Forms.Button();
+            this.openFileDialog_CSV = new System.Windows.Forms.OpenFileDialog();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.ErrorInfo = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(320, 33);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse the input CSV file";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(67, 33);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(219, 22);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // ErrorInfo
+            // 
+            this.ErrorInfo.Location = new System.Drawing.Point(0, 0);
+            this.ErrorInfo.Name = "ErrorInfo";
+            this.ErrorInfo.Size = new System.Drawing.Size(100, 23);
+            this.ErrorInfo.TabIndex = 0;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(28, 102);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(31, 17);
+            this.labelInfo.TabIndex = 2;
+            this.labelInfo.Text = "Info";
+            this.labelInfo.Visible = false;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(124, 151);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(95, 38);
+            this.buttonOk.TabIndex = 3;
+            this.buttonOk.Text = "ok";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Visible = false;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // LoadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 332);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.ErrorInfo);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.Browse);
+            this.Name = "LoadForm";
+            this.Text = "Load CSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_CSV;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label ErrorInfo;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonOk;
+    }
+}
+
--- src/LoadCSV/LoadForm.cs
+++ src/LoadCSV/LoadForm.cs
@@ -1,1 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using CsvHelper;
+
+
+namespace LoadCSV
+{
+    enum TypesOfRecords
+    {
+        
+        phoneNumber,
+        email,
+        number,
+        stirng_value
+    }
+    public partial class LoadForm : Form
+    {
+        static readonly log4net.ILog s_Logger = log4net.LogManager.GetLogger(typeof(LoadForm));
+        const string checkFile = "Please check if the file you  are trying to load is open by another process or deleted";
+        const string filePathForXml = "Please find the converted xmls under same path as input file exists";
+        const string conversionError = "Can not convert to xml";
+        public LoadForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            openFileDialog_CSV.ShowDialog();
+            openFileDialog_CSV.InitialDirectory = @"C:\";
+            openFileDialog_CSV.CheckFileExists = true;
+            openFileDialog_CSV.CheckPathExists = true;
+            openFileDialog_CSV.ReadOnlyChecked = true;
+            openFileDialog_CSV.Filter = "csv files (*.csv)|*.csv";
+            txtFileName.Text = openFileDialog_CSV.FileName;
+            WriteCSVContentToXml(txtFileName.Text);
+           
+
+        }
+
+
+        public void WriteCSVContentToXml(string filePath)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                using (CsvReader readerCsv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    dynamic rows = readerCsv.GetRecords<dynamic>();
+                     foreach (var row in rows)
+                    {
+                        List<string> fields = new List<string>();
+                        List<string> headers = new List<string>();
+                        IDictionary<string, object> rowValues = row;
+                        foreach (var val in rowValues)
+                        {
+                            headers.Add(val.Key.ToString());
+                        }
+                        foreach (var val in rowValues)
+                        {
+                            fields.Add(val.Value.ToString());
+                        }
+                        var dirName = Path.GetDirectoryName(filePath);
+                        string path = dirName +@"\" + fields[0] + ".txt";
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            for (int j = 0; j < fields.Count; j++)
+                            {
+                                string type = TypesOfRecords.stirng_value.ToString();
+                                 if (PhoneNumber.IsPhoneNbr(fields[j]))
+                                  {
+                                    type = TypesOfRecords.phoneNumber.ToString();
+                                   }
+                                else if (EMail.IsEmail(fields[j]))
+                                {
+                                    type = TypesOfRecords.email.ToString();
+                                }
+                                else if (int.TryParse(fields[j], out int res))
+                                {
+                                    type = TypesOfRecords.number.ToString();
+                                }
+                                XElement node = new XElement(
+                                        new XElement(headers[j].Replace(" ", ""),
+                                         new XElement("Value", fields[j]),
+                                         new XElement("Type", type))
+                                         );
+                                sw.Write(node.ToString() + "\n");
+                            }
+                        }
+                    }
+                }
+                labelInfo.Visible = true;
+                labelInfo.Text = filePathForXml;
+                buttonOk.Visible = true;
+            }
+
+            catch (IOException ex)
+            {
+                labelInfo.Visible = true;
+                labelInfo.Text = checkFile;
+                buttonOk.Visible = true;
+                s_Logger.Error("Erro message:" + ex.Message.ToString(), ex);
+            }
+            catch (Exception ex)
+            {
+                labelInfo.Visible = true;
+                labelInfo.Text = conversionError;
+                buttonOk.Visible = true;
+                s_Logger.Error("Erro message:" + ex.Message.ToString(), ex);
+            }
+
+
+        }
+                private void buttonOk_Click(object sender, EventArgs e)
+                {
+                    if (labelInfo.Text == checkFile)
+                    {
+                        labelInfo.Visible = false;
+                        buttonOk.Visible = false;
+
+                    }
+                    else
+                    {
+                       // buttonOk.Visible = true;
+                        this.Close();
+                    }
+                }
+            }
+        }
 
